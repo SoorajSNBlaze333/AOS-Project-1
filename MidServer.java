1c223a3d34886a6ee3957f61ec9fefa465ab1bf7
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2022-02-26 15:52:44 -0600

    Added data in and out

--- MidServer.java
+++ MidServer.java
@@ -1,22 +1,114 @@
-// import java.io.BufferedReader;
-// import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Auth {
+  public String filename = "";
+  public String username = "";
+  public String password = "";
+  public String role = "";
+  public Boolean hasLoggedIn = false;
+  public String serverGroup = "";
+  
+  public Auth(String filename) {
+    this.filename = filename;
+  }
+
+  public void login() {
+    try {
+      File authFile = new File(this.filename);
+      Scanner myReader = new Scanner(authFile);  
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    } 
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
+
 public class MidServer {
   public static void main(String[] args) {
+    Auth user = new Auth("userList.txt");
     try {
       int port = Integer.parseInt(args[0]);
       ServerSocket serverSocket = new ServerSocket(port);
       Socket socket = serverSocket.accept();
 
-      // BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-      PrintWriter out = new PrintWriter(socket.getOutputStream());
+      DataInputStream in = new DataInputStream(socket.getInputStream());
+      DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+      // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      out.write("Message from server");
+      out.writeUTF("100");
       out.flush();
 
+      String messageFromClient = in.readUTF();
+      int username = 0;
+      int password = 0;
+      // String message = "";
+      while(messageFromClient.length() > 0) {
+        System.out.println(messageFromClient); // -> debug statement
+        // message = br.readLine();
+        if (messageFromClient.equals("login")) {
+          if (user.role.length() > 0) out.writeUTF("logged-in");
+          else {
+            if (username == 0) {
+              username = 1;
+              out.writeUTF("get-username");
+            }
+          }
+          out.flush();
+        }
+        if (messageFromClient.equals("items")) {
+          if (user.role.length() > 0) { /*code here to connect to group server*/ }
+          else {
+            out.writeUTF("401");
+            out.flush();
+          }
+        } else {
+          if (username == 1) {
+            user.setUsername(messageFromClient);
+            username = 0;
+            password = 1;
+            out.writeUTF("get-password");
+            out.flush();
+          } else if (username == 0 && password == 1) {
+            user.setPassword(messageFromClient);
+            password = 0;
+            out.writeUTF("200");
+            out.flush();
+          } else {
+            out.writeUTF("200");
+            out.flush();
+          }
+        }
+        messageFromClient = in.readUTF();
+        // if (user.hasLoggedIn == false && clientMessage.contains("username-")) {
+        //   user.setUsername(clientMessage);
+        // }
+        // if (user.hasLoggedIn == false && clientMessage.contains("password-")) {
+        //   user.setPassword(clientMessage);
+        // }
+
+      }
+
+      in.close();
       out.close();
       serverSocket.close();
     } catch(Exception e) {
--- Sender.java
+++ Sender.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-// import java.io.PrintStream;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -9,8 +11,6 @@ public class Sender {
     if (args.length < 1) {
       System.out.println("Please provide the IP Address and Port Number");
     }
-    // String username = "";
-    // String password = "";
     try {
       InetAddress ipAddress = InetAddress.getByName(args[0]);
       int port = Integer.parseInt(args[1]);
@@ -18,16 +18,54 @@ public class Sender {
       try {
         Socket socket = new Socket(ipAddress, port);
 
-        // PrintStream out = new PrintStream(socket.getOutputStream());
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = in.readLine();
-        while( line != null ) {
-            System.out.println( line );
-            line = in.readLine();
+        String messageFromServer = in.readUTF();
+        String message = "";
+        
+        while(!messageFromServer.equals("connection-closed")) {
+          System.out.println(messageFromServer); // -> debug statement
+          if (messageFromServer.equals("100")) {
+            System.out.println("Connection established with server!");
+          }
+          if (messageFromServer.equals("get-username")) {
+            System.out.println("Enter the username");
+          }
+          if (messageFromServer.equals("get-password")) {
+            System.out.println("Enter the password");
+          }
+          if (messageFromServer.equals("401")) {
+            System.out.println("Please login to the server!!");
+          }
+          if (messageFromServer.equals("logged-in")) {
+            System.out.println("You are already logged in to the server!!");
+          }
+          message = br.readLine();
+          out.writeUTF(message);
+
+          // if (serverMessage.equals("500")) {
+          //   System.out.println("Server error. Please try again later!!");
+          // }
+          // String username = getUsername(scanner);
+          // String password = getPassword(scanner);
+          // out.println("username-" + username);
+          // out.flush();
+          // out.println("password-" + password);
+          // out.flush();
+
+          // String input = getUserInput(scanner);
+          // while(!input.equals("exit")) {
+          //   out.println(input);
+          //   input = getUserInput(scanner);
+          // }
+          // break; --> debugging line
+          messageFromServer = in.readUTF();
         }
 
         in.close();
+        out.close();
         socket.close();
       } catch (Exception e) { // error handling for socket programming
         e.printStackTrace(); 
